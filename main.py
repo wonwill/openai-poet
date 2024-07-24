64c0391005813373e6d9238fee5f2d16c92d15ed
Won Gi Yoo <wongi.yoo@gmail.com>
2024-07-24 09:12:26 +0900

    main.py
    back to my code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# from dotenv import load_dotenv
-# load_dotenv()
+'''
+#jocoding original script - error
 
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
@@ -7,15 +7,14 @@ from langchain.chat_models import ChatOpenAI
 chat_model = ChatOpenAI()
 
 st.title('인공지능 시인')
-
 content = st.text_input('시의 주제를 제시해주세요.')
 
 if st.button('시 작성 요청하기'):
     with st.spinner('시 작성 중...'):
         result = chat_model.predict(content + "에 대한 시를 써줘")
-        st.write(result)
-        
+        st.write(result)     
 '''
+
 import streamlit as st
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
